32cf2be3f5d3f2ad47ab9ff0537deec2ba8d3435
Vitscuira <jvscuira@gmail.com>
2022-04-13 19:12:58 -0300

    Add CSS

--- media/scripts.js
+++ media/scripts.js
@@ -1,22 +1,22 @@
-function calcularMedia(){
 
+
+function calcularMedia(){
     let nota1 = parseFloat(document.getElementById("nota1").value);
     let nota2 = parseFloat(document.getElementById("nota2").value);
     let media = (nota1+nota2)/2;
-
     const para = document.createElement('div');
-
     para.textContent = media;
 
-    document.getElementById("resultado").appendChild(para);
-
-    if (media>=6){
-        para.textContent = "Aprovado! Média: " + media;
-        document.getElementById("resultado").appendChild(para);
+    if (!nota1 && !nota2 ){
+        alert("Preencha os dois campos!");
     }else{
-        para.textContent = "Reprovado! Média: " + media;
         document.getElementById("resultado").appendChild(para);
+        if (media>=6){
+            para.textContent = "Aprovado! Média: " + media;
+            document.getElementById("resultado").appendChild(para);
+        }else{
+            para.textContent = "Reprovado! Média: " + media;
+            document.getElementById("resultado").appendChild(para);
+        }
     }
-
-
 }
